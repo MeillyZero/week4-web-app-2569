37a6517db78c2ee61dfb1a0345a9ef28a6978f56
MeillyZero <meii1524365@gmail.com>
2026-06-14 12:47:29 +0700

    first commit

---
+++ func102.js
@@ -0,0 +1,7 @@
+function myName(name, age) {
+    //console.log(`My name is ${name}. Age: ${age}`);
+    nameValue = `My name is ${name}. Age: ${age}`;
+    return name;
+}
+
+console.log(myName("Meilly", 17));
